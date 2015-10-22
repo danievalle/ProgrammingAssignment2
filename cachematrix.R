c41382cac9f797df3e0e9c4a118a9c4e9f579b4d
danievalle <daniel.valle@nestle.com>
2015-10-22 15:22:19 +0200

    Update cachematrix.R
    update comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,20 +5,18 @@
 ## 4. get the value of the solve
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## Return an object List that can cache the inverse of a matrix (in parameter)
+    ## Return an object List (getter and setter) that can cache the inverse of a matrix (in parameter)
     ## Parameter x has to be a Matrix
     
     m <- NULL
     
-    # assign to "set" a function which set the value of the matrix
-    # the matrix is the parameter (y)
+    # Assign to "set" a function which set the value of the matrix
+    # passed as input parameter
     set <- function(y) {
-        # if the parameter (y) is a matrix
-        # assign a values (y and null) to objects (x and m) in an environment
-        # that is different from the current environment
+        # If the parameter (y) is a matrix
+        # cahe the matrix in 'x' (in a different environment)
+        # and clear the inverted matrix already calculated
         if (is.matrix(y)){
-            # if "set" is called, matrix is replaced by parameter
-            # and the inverted matrix already calculated is removed from the cache
             x <<- y
             m <<- NULL
         } else{
@@ -26,17 +24,17 @@ makeCacheMatrix <- function(x = matrix()) {
         }
     }
     
-    # assign to "get" a function returning the matrix of the current obect
+    # Assign to "get" a function returning the matrix of the current obect
     get <- function() x
     
-    # cache the inverted matrix.
+    # Assign to 'setsolve' a function which caches the inverted matrix.
     # solve is the inverted matrix (parameter of the function) which is cached into m
     setsolve <- function(solve) m <<- solve
     
-    # a function returning "m" (inverted matrix)
+    # Assign to 'getsolve' a function returning "m" (inverted matrix)
     getsolve <- function() m
     
-    # return list with getter and setter of the object
+    # Return list with getter and setter of the object
     list(set = set,
          get = get,
          setsolve = setsolve,
@@ -52,32 +50,24 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     ## 'x' has to be a special matrix returned by 'makeCacheMatrix'.
     
-    # if parameter (x) is a matrix and not a "special matrix", give an error message to user
+    # If parameter (x) is a matrix and not a "special matrix", give an error message to user
     # and return NULL
     if(is.matrix(x)){
-        message("parameter has to be a special CacheMatrix") 
-        # not requested in the function, so return NULL
-        # x <- makeCacheMatrix(x)
+        message("parameter has to be a special CacheMatrix")
         return(NULL)
     }
     
-    # check if inverted matrix is cached
-    # if it is already in the cache the inverted matrix is returned and function stops
+
+    # If it is already in the cache the inverted matrix is returned and function stops
     m <- x$getsolve()
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
     
-    # assign to "data" the matrix to solve
-    data <- x$get()
-    
-    # compute into to "m" the invert of the matrix
-    m <- solve(data, ...)
-    
-    # cache the "m" inverted matrix
-    x$setsolve(m)
-    
-    # return the inverted matrix
-    m
+    # Else invert and return
+    data <- x$get()         # assign to "data" the matrix to solve
+    m <- solve(data, ...)   # compute into to "m" the invert of the matrix
+    x$setsolve(m)           # cache the "m" inverted matrix
+    m                       # return the inverted matrix
 }
